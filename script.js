642d1ea59ea9096b08ec89d6124d6a82a638b95d
nido <56178638+doniokta@users.noreply.github.com>
2024-10-12 19:33:02 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const textElement = document.getElementById("typing-text");
-  const text = "IT Support and IT Technician";
-  let index = 0;
+<p id="typing-text" class="text-lg md:text-xl mb-4"></p>
+<script>
+  document.addEventListener("DOMContentLoaded", () => {
+    const textElement = document.getElementById("typing-text");
+    const text = "IT Support and IT Technician";
+    let index = 0;
 
-  function type() {
-    if (index < text.length) {
-      textElement.textContent += text.charAt(index);
-      index++;
-      setTimeout(type, 100); // Adjust typing speed here
+    function type() {
+      if (index < text.length) {
+        textElement.textContent += text.charAt(index);
+        index++;
+        setTimeout(type, 100); // Adjust typing speed here
+      }
     }
-  }
 
-  type();
-});
+    type();
+  });
+</script>
